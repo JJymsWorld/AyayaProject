fcc423b2900c5e4b4a7fdb03b692ce5960e632f9
Jyms <qq171895821@163.com>
2021-04-20 15:58:49 +0800

    modify some page http request

--- pages/HotActivities/EventsPage.vue
+++ pages/HotActivities/EventsPage.vue
@@ -24,19 +24,19 @@
 				<uni-list class="Events-list-list" :border="false">
 					<uni-list-item :border="false" class="Events-list-item" v-for="(item,index) in EventsList" :key="index" direction="column" :title="item.title" :ellipsis="2" to="../works/works">
 						<view slot="header" class="Events-list-item-header">
-							<image :src="item.img_url" mode="aspectFill"></image>
+							<image :src="item.photo" mode="aspectFill"></image>
 						</view>
 						<view slot="body" class="Events-list-item-body">
-							<text>{{item.title}}</text>
+							<text>{{item.content}}</text>
 						</view>
 						<view slot="footer" class="Events-list-item-footer">
 							<view class="Events-list-item-footer-left">
-								<view class="Events-list-item-footer-EventsActive" v-if="item.isActive==true">进行中</view>
-								<view class="Events-list-item-footer-EventsnoActive" v-if="item.isActive==false">已结束</view>
+								<view class="Events-list-item-footer-EventsActive" v-if="item.state==1">进行中</view>
+								<view class="Events-list-item-footer-EventsnoActive" v-if="item.state==0">已结束</view>
 							</view>
 							<view class="Events-list-item-footer-right">
 								<view :class="['far','fa-user']" class="Events-list-item-footer-right-icon"></view>
-								<text class="Events-list-item-footer-right-text">{{item.AttendentNum}}参与</text>
+								<text class="Events-list-item-footer-right-text">{{item.number}}参与</text>
 							</view>
 						</view>
 					</uni-list-item>
@@ -49,31 +49,39 @@
 
 <script>
 	export default{
+		onLoad() {
+			const http = new this.$Request();
+			http.get('/Activity/Events/getAllEvents').then(res=>{
+				this.EventsList = res.data;
+			}).catch(err=>{
+				console.log(err);
+			})
+		},
 		data(){
 			return {
 				filterList:['全部','Cos','JK','汉服','Lolita','妆容'],
 				EventsList:[
-					{
-						id:0,
-						img_url:"../../static/EventsSource/1.jpg",
-						title:"【Cos正片征集】第10-9期《请问您今天要来点兔子吗？》保登心爱",
-						isActive:true,
-						AttendentNum:1920
-					},
-					{
-						id:2,
-						img_url:"../../static/EventsSource/2.jpg",
-						title:"【Cos正片征集】第10-1期《请问您金条要来天兔子吗？》宇治松 千夜",
-						isActive:false,
-						AttendentNum:1920
-					},
-					{
-						id:3,
-						img_url:"../../static/EventsSource/2.jpg",
-						title:"【Cos合集】第十期《请问您今天要来点兔子吗？》",
-						isActive:true,
-						AttendentNum:1920
-					}
+					// {
+					// 	id:0,
+					// 	img_url:"../../static/EventsSource/1.jpg",
+					// 	title:"【Cos正片征集】第10-9期《请问您今天要来点兔子吗？》保登心爱",
+					// 	isActive:true,
+					// 	AttendentNum:1920
+					// },
+					// {
+					// 	id:2,
+					// 	img_url:"../../static/EventsSource/2.jpg",
+					// 	title:"【Cos正片征集】第10-1期《请问您金条要来天兔子吗？》宇治松 千夜",
+					// 	isActive:false,
+					// 	AttendentNum:1920
+					// },
+					// {
+					// 	id:3,
+					// 	img_url:"../../static/EventsSource/2.jpg",
+					// 	title:"【Cos合集】第十期《请问您今天要来点兔子吗？》",
+					// 	isActive:true,
+					// 	AttendentNum:1920
+					// }
 				]
 			}
 		},
